4248fbf4bfa787fbddb7510877faa16b5904cd55
= <=>
2013-06-30 02:12:33 +0200

    create graphocs

--- BatteryDaydream/src/main/java/pl/patrykgrzegorczyk/batterydaydream/service/BatteryDreamService.java
+++ BatteryDaydream/src/main/java/pl/patrykgrzegorczyk/batterydaydream/service/BatteryDreamService.java
@@ -3,6 +3,8 @@ package pl.patrykgrzegorczyk.batterydaydream.service;
 import android.service.dreams.DreamService;
 import android.util.Log;
 import android.widget.TextView;
+import android.widget.ProgressBar;
+import android.graphics.Color;
 
 import pl.patrykgrzegorczyk.batterydaydream.BatteryMonitor;
 import pl.patrykgrzegorczyk.batterydaydream.R;
@@ -16,6 +18,8 @@ public class BatteryDreamService extends DreamService implements BatteryMonitor.
 
     private BatteryMonitor mBatteryMonitor;
     private TextView mBatteryLevelView;
+    private TextView mBatteryLevelView2;
+    private ProgressBar mBatteryProgressBar;
 
     @Override
     public void onAttachedToWindow() {
@@ -34,6 +38,9 @@ public class BatteryDreamService extends DreamService implements BatteryMonitor.
         setContentView(R.layout.battery_daydream);
 
         mBatteryLevelView = (TextView) findViewById(R.id.battery_level);
+        mBatteryLevelView2 = (TextView) findViewById(R.id.battery_level_2);
+
+        mBatteryProgressBar = (ProgressBar) findViewById(R.id.battery_progress);
     }
 
     @Override
@@ -61,6 +68,33 @@ public class BatteryDreamService extends DreamService implements BatteryMonitor.
     @Override
     public void onBatteryLevelChanged(int batteryLevel) {
         //Update battery level info
-        mBatteryLevelView.setText(String.valueOf(batteryLevel));
+
+        //batteryLevel = 1;
+
+        String batteryLevelText = String.valueOf(batteryLevel);
+
+        String firstText = batteryLevelText.substring(0, 1);
+        String secondText = batteryLevelText.substring(1);
+
+        mBatteryLevelView.setText(firstText);
+        mBatteryLevelView2.setText(secondText);
+
+        if(batteryLevel <= 10){
+            mBatteryLevelView.setTextColor(Color.RED);
+            mBatteryLevelView2.setTextColor(Color.RED);
+        }else{
+            mBatteryLevelView.setTextColor(Color.WHITE);
+            mBatteryLevelView2.setTextColor(Color.WHITE);
+        }
+
+        if(batteryLevel == 100){
+            mBatteryLevelView.setTextColor(Color.rgb(1,1,1));
+            mBatteryLevelView2.setTextColor(Color.rgb(1,1,1));
+            mBatteryProgressBar.setProgress(batteryLevel);
+        }
+        else{
+            mBatteryProgressBar.setSecondaryProgress(batteryLevel);
+        }
+
     }
 }
