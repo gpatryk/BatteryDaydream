c7246561fb631a94065f79a0625d47f23f7ecbe5
Patryk Grzegorczyk <patryk.grzegorczyk@gmail.com>
2013-07-05 17:23:50 +0200

    Better approach to set listpreference summary

--- BatteryDaydream/src/main/java/pl/patrykgrzegorczyk/batterydaydream/fragment/DefaultPreferenceFragment.java
+++ BatteryDaydream/src/main/java/pl/patrykgrzegorczyk/batterydaydream/fragment/DefaultPreferenceFragment.java
@@ -1,8 +1,8 @@
 package pl.patrykgrzegorczyk.batterydaydream.fragment;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.ListPreference;
-import android.preference.Preference;
 import android.preference.PreferenceFragment;
 
 import pl.patrykgrzegorczyk.batterydaydream.R;
@@ -10,7 +10,7 @@ import pl.patrykgrzegorczyk.batterydaydream.R;
 /**
  * Fragment with preferences
  */
-public class DefaultPreferenceFragment extends PreferenceFragment implements Preference.OnPreferenceChangeListener {
+public class DefaultPreferenceFragment extends PreferenceFragment implements SharedPreferences.OnSharedPreferenceChangeListener {
 
     //This constants should be in sync with preferences.xml
     public static final String KEY_NORMAL_BRIGHTNESS_MODE = "normal_brightness_mode";
@@ -22,45 +22,15 @@ public class DefaultPreferenceFragment extends PreferenceFragment implements Pre
 
         addPreferencesFromResource(R.xml.preferences);
 
-        ListPreference animationType = (ListPreference) findPreference(KEY_ANIMATION_TYPE);
-        animationType.setOnPreferenceChangeListener(this);
-
-        updateAnimationTypePreferenceSummary(KEY_ANIMATION_TYPE, animationType.getValue());
+        getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
     }
 
     @Override
-    public boolean onPreferenceChange(Preference preference, Object newValue) {
-        String key = preference.getKey();
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
         if(key.equals(KEY_ANIMATION_TYPE)) {
-            updateAnimationTypePreferenceSummary(key, newValue.toString());
+            //We have to update summary manually because of https://code.google.com/p/android/issues/detail?id=27867
+            ListPreference preference = (ListPreference) findPreference(key);
+            preference.setSummary(preference.getEntry());
         }
-        return true;
-    }
-    private void updateAnimationTypePreferenceSummary(String key, String value) {
-        String[] values = getResources().getStringArray(R.array.animation_types_values);
-        String[] names = getResources().getStringArray(R.array.animation_types_names);
-
-        int valueIndex = -1;
-
-        for(int i = 0; i < values.length; i++) {
-            if(values[i].equals(value)) {
-                valueIndex = i;
-                break;
-            }
-        }
-
-        if(valueIndex < 0) {
-            //Value not found in array
-            return;
-        }
-
-        if(valueIndex >= names.length) {
-            //Index grater than max index in names array
-            return;
-        }
-
-        String name = names[valueIndex];
-
-        findPreference(key).setSummary(name);
     }
 }
